d3abbfa7827f4f8345f065d2c8c72b17515bef8b
MaxXSoft <x@MaxXSoft.net>
2019-08-09 22:05:24 +0800

    (unit) added interface 'UnitInterface'

---
+++ src/core/unit/unit.h
@@ -0,0 +1,24 @@
+#ifndef RISKY32_CORE_UNIT_UNIT_H_
+#define RISKY32_CORE_UNIT_UNIT_H_
+
+#include <memory>
+
+#include "define/inst.h"
+#include "core/state.h"
+
+class UnitInterface {
+ public:
+  virtual ~UnitInterface() = default;
+  // execute a R-type instruction
+  virtual void ExecuteR(const InstR &inst, CoreState &state) = 0;
+  // execute a I-type instruction
+  virtual void ExecuteI(const InstI &inst, CoreState &state) = 0;
+  // execute a S-type instruction
+  virtual void ExecuteS(const InstS &inst, CoreState &state) = 0;
+  // execute a U-type instruction
+  virtual void ExecuteU(const InstU &inst, CoreState &state) = 0;
+};
+
+using UnitPtr = std::shared_ptr<UnitInterface>;
+
+#endif  // RISKY32_CORE_UNIT_UNIT_H_
