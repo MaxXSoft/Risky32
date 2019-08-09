a65c61ea1cdfbeafa7ec59f7115f92f75b0f4c93
MaxXSoft <x@MaxXSoft.net>
2019-08-09 15:37:59 +0800

    (define) added header 'define.h'

---
+++ src/define/define.h
@@ -0,0 +1,9 @@
+#ifndef RISKY32_DEFINE_DEFINE_H_
+#define RISKY32_DEFINE_DEFINE_H_
+
+#include <cstdint>
+
+// reset vector of all harts
+constexpr std::uint32_t kResetVector = 0x00001000;
+
+#endif  // RISKY32_DEFINE_DEFINE_H_
