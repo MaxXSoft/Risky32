4aaf364d847ae1579db99c90d250597ca49b0468
MaxXSoft <x@MaxXSoft.net>
2019-08-09 22:39:09 +0800

    (int, lsu, branch, system) added 4 functional units

---
+++ src/core/unit/system.h
@@ -0,0 +1,14 @@
+#ifndef RISKY32_CORE_UNIT_SYSTEM_H_
+#define RISKY32_CORE_UNIT_SYSTEM_H_
+
+#include "core/unit/unit.h"
+
+class SystemUnit : public UnitInterface {
+ public:
+  void ExecuteR(const InstR &inst, CoreState &state) override;
+  void ExecuteI(const InstI &inst, CoreState &state) override;
+  void ExecuteS(const InstS &inst, CoreState &state) override;
+  void ExecuteU(const InstU &inst, CoreState &state) override;
+};
+
+#endif  // RISKY32_CORE_UNIT_SYSTEM_H_
