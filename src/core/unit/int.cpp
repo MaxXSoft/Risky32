59f85d72959b7e7f38dccbe6ff84787c6f94f9ec
MaxXSoft <x@MaxXSoft.net>
2019-11-17 18:53:44 +0800

    (int) added support of M extension

--- src/core/unit/int.cpp
+++ src/core/unit/int.cpp
@@ -7,31 +7,80 @@
 namespace {
 
 std::uint32_t PerformIntOp(std::uint32_t opr1, std::uint32_t opr2,
-                           std::uint32_t funct3, bool is_type_2) {
-  switch (funct3) {
-    case kADDSUB: return is_type_2 ? opr1 - opr2 : opr1 + opr2;
-    case kSLL: return opr1 << (opr2 & 0b11111);
-    case kSLT: return static_cast<std::int32_t>(opr1) <
-                      static_cast<std::int32_t>(opr2);
-    case kSLTU: return opr1 < opr2;
-    case kXOR: return opr1 ^ opr2;
-    case kSRX: {
-      if (is_type_2) {
-        // 'SRA'
-        return static_cast<std::int32_t>(opr1) >> (opr2 & 0b11111);
+                           std::uint32_t funct3, std::uint32_t funct7) {
+  if (funct7 == kRV32M) {
+    // signed operands
+    std::int32_t opr1s = opr1, opr2s = opr2;
+    std::uint64_t ans;
+    // multiplication & division
+    switch (funct3) {
+      case kMUL: return (static_cast<std::int64_t>(opr1s) *
+                        static_cast<std::int64_t>(opr2s)) & 0xffffffff;
+      case kMULH: return (static_cast<std::int64_t>(opr1s) *
+                         static_cast<std::int64_t>(opr2s)) >> 32;
+      case kMULHSU: return (static_cast<std::int64_t>(opr1s) *
+                           static_cast<std::uint64_t>(opr2)) >> 32;
+      case kMULHU: return (static_cast<std::uint64_t>(opr1) *
+                          static_cast<std::uint64_t>(opr2)) >> 32;
+      case kDIV: {
+        if (!opr2) {
+          // division by zero
+          return 0xffffffff;
+        }
+        else if (opr1 == 0x80000000 && opr2s == -1) {
+          // signed overflow
+          return 0x80000000;
+        }
+        else {
+          // normal division
+          return opr1s / opr2s;
+        }
       }
-      else {
-        // 'SRL'
-        return opr1 >> (opr2 & 0b11111);
+      case kDIVU: return !opr2 ? 0xffffffff : opr1 / opr2;
+      case kREM: {
+        if (!opr2) {
+          // division by zero
+          return opr1;
+        }
+        else if (opr1 == 0x80000000 && opr2s == -1) {
+          // signed overflow
+          return 0;
+        }
+        else {
+          // normal division
+          return opr1s % opr2s;
+        }
       }
-    }
-    case kOR: return opr1 | opr2;
-    case kAND: return opr1 & opr2;
-    default: {
+      case kREMU: return !opr2 ? opr1 : opr1 % opr2;
       // unreachable situation
       // because 'funct3' only has 3 bits and
       // all 8 cases have been handled before
-      assert(false);
+      default: assert(false);
+    }
+  }
+  else {
+    // integer operations
+    switch (funct3) {
+      case kADDSUB: return funct7 == kRV32I2 ? opr1 - opr2 : opr1 + opr2;
+      case kSLL: return opr1 << (opr2 & 0b11111);
+      case kSLT: return static_cast<std::int32_t>(opr1) <
+                        static_cast<std::int32_t>(opr2);
+      case kSLTU: return opr1 < opr2;
+      case kXOR: return opr1 ^ opr2;
+      case kSRX: {
+        if (funct7 == kRV32I2) {
+          // 'SRA'
+          return static_cast<std::int32_t>(opr1) >> (opr2 & 0b11111);
+        }
+        else {
+          // 'SRL'
+          return opr1 >> (opr2 & 0b11111);
+        }
+      }
+      case kOR: return opr1 | opr2;
+      case kAND: return opr1 & opr2;
+      // same as above
+      default: assert(false);
     }
   }
 }
@@ -50,20 +99,14 @@ void IntUnit::ExecuteR(const InstR &inst, CoreState &state) {
   else {
     opr2 = state.regs[inst.rs2];
   }
-  // get instruction type
-  bool is_type_2;
-  if (inst.funct7 == kRV32I1) {
-    is_type_2 = false;
-  }
-  else if (inst.funct7 == kRV32I2) {
-    is_type_2 = true;
-  }
-  else {
+  // check 'funct7' field
+  if (inst.funct7 != kRV32I1 && inst.funct7 != kRV32I2 &&
+      inst.funct7 != kRV32M) {
     // invalid 'funct7' field
     RaiseException(kExcIllegalInst, state);
   }
   // calculate
-  state.regs[inst.rd] = PerformIntOp(opr1, opr2, inst.funct3, is_type_2);
+  state.regs[inst.rd] = PerformIntOp(opr1, opr2, inst.funct3, inst.funct7);
 }
 
 void IntUnit::ExecuteI(const InstI &inst, CoreState &state) {
@@ -73,7 +116,7 @@ void IntUnit::ExecuteI(const InstI &inst, CoreState &state) {
   // sign-extended
   auto opr2 = inst.imm & 0x800 ? 0xfffff000 | inst.imm : inst.imm;
   // calculate
-  state.regs[inst.rd] = PerformIntOp(opr1, opr2, inst.funct3, false);
+  state.regs[inst.rd] = PerformIntOp(opr1, opr2, inst.funct3, kRV32I1);
 }
 
 void IntUnit::ExecuteS(const InstS &inst, CoreState &state) {
