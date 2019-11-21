beed0d37c148d30931b395dc99b5fce5b88ddd49
MaxXSoft <x@MaxXSoft.net>
2019-11-21 23:27:03 +0800

    (version) added definitions of version info

---
+++ src/version.h
@@ -0,0 +1,26 @@
+#ifndef RISKY32_VERSION_H_
+#define RISKY32_VERSION_H_
+
+// version information of Risky32
+
+#ifndef APP_NAME
+#define APP_NAME            "Risky32"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif  // RISKY32_VERSION_H_
