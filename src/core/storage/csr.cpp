ef4e5a837a884c927b7a942c2b9b97f8eb0e949a
MaxXSoft <x@MaxXSoft.net>
2019-11-27 23:30:48 +0800

    (csr) modified more details

--- src/core/storage/csr.cpp
+++ src/core/storage/csr.cpp
@@ -1,21 +1,114 @@
 #include "core/storage/csr.h"
 
-#include "define/csr.h"
-
 namespace {
 
 //
 
 }  // namespace
 
+void CSR::InitCSR() {
+  // TODO
+}
+
+void CSR::InitMapping() {
+  // user mode CSRs
+  csrs_[kCSRCycle]      = IntPtrCast<32>(&mcycle_);
+  csrs_[kCSRInstRet]    = IntPtrCast<32>(&minstret_);
+  csrs_[kCSRCycleH]     = IntPtrCast<32>(&mcycle_) + 1;
+  csrs_[kCSRInstRetH]   = IntPtrCast<32>(&minstret_) + 1;
+  // supervisor mode CSRs
+  csrs_[kCSRSStatus]    = &sstatus_;
+  csrs_[kCSRSIE]        = &zero_;
+  csrs_[kCSRSTVec]      = &zero_;
+  csrs_[kCSRSCounterEn] = &zero_;
+  csrs_[kCSRSScratch]   = &sscratch_;
+  csrs_[kCSRSEPC]       = &sepc_;
+  csrs_[kCSRSCause]     = &zero_;
+  csrs_[kCSRSTVal]      = &zero_;
+  csrs_[kCSRSIP]        = &zero_;
+  csrs_[kCSRSATP]       = &satp_;
+  // machine mode CSRs
+  csrs_[kCSRMVenderId]  = &zero_;
+  csrs_[kCSRMArchId]    = &zero_;
+  csrs_[kCSRMImpId]     = &zero_;
+  csrs_[kCSRMHartId]    = &zero_;
+  csrs_[kCSRMStatus]    = &mstatus_;
+  csrs_[kCSRMISA]       = &misa_;
+  csrs_[kCSRMIE]        = &mie_;
+  csrs_[kCSRMTVec]      = &mtvec_;
+  csrs_[kCSRMCounterEn] = &zero_;
+  csrs_[kCSRMScratch]   = &mscratch_;
+  csrs_[kCSRMEPC]       = &mepc_;
+  csrs_[kCSRMCause]     = &mcause_;
+  csrs_[kCSRMTVal]      = &mtval_;
+  csrs_[kCSRMIP]        = &mip_;
+  csrs_[kCSRPMPCfg0]    = &zero_;
+  csrs_[kCSRPMPCfg1]    = &zero_;
+  csrs_[kCSRPMPCfg2]    = &zero_;
+  csrs_[kCSRPMPCfg3]    = &zero_;
+  csrs_[kCSRPMPAddr0]   = &zero_;
+  csrs_[kCSRPMPAddr1]   = &zero_;
+  csrs_[kCSRPMPAddr2]   = &zero_;
+  csrs_[kCSRPMPAddr3]   = &zero_;
+  csrs_[kCSRPMPAddr4]   = &zero_;
+  csrs_[kCSRPMPAddr5]   = &zero_;
+  csrs_[kCSRPMPAddr6]   = &zero_;
+  csrs_[kCSRPMPAddr7]   = &zero_;
+  csrs_[kCSRPMPAddr8]   = &zero_;
+  csrs_[kCSRPMPAddr9]   = &zero_;
+  csrs_[kCSRPMPAddr10]  = &zero_;
+  csrs_[kCSRPMPAddr11]  = &zero_;
+  csrs_[kCSRPMPAddr12]  = &zero_;
+  csrs_[kCSRPMPAddr13]  = &zero_;
+  csrs_[kCSRPMPAddr14]  = &zero_;
+  csrs_[kCSRPMPAddr15]  = &zero_;
+  csrs_[kMCycle]        = IntPtrCast<32>(&mcycle_);
+  csrs_[kMInstRet]      = IntPtrCast<32>(&minstret_);
+  csrs_[kMCycleH]       = IntPtrCast<32>(&mcycle_) + 1;
+  csrs_[kMInstRetH]     = IntPtrCast<32>(&minstret_) + 1;
+  csrs_[kMCountInhibit] = &zero_;
+}
+
 CSR::CSR() {
-  // TODO
+  // current privilege level is M
+  cur_priv_ = kPrivLevelM;
+  // initialize CSR & mapping
+  InitCSR();
+  InitMapping();
 }
 
-std::uint32_t CSR::ReadData(std::uint32_t addr) {
-  // TODO
+void CSR::UpdateCounter() {
+  ++mcycle_;
+  ++minstret_;
 }
 
-void CSR::WriteData(std::uint32_t addr, std::uint32_t value) {
-  // TODO
+bool CSR::ReadData(std::uint32_t addr, std::uint32_t &value) {
+  // TODO: deal with privilege! (lower cannot access upper)
+  auto it = csrs_.find(addr);
+  if (it == csrs_.end()) {
+    // CSR does not exist, illegal instruction
+    return false;
+  }
+  else {
+    value = *it->second;
+    return true;
+  }
+}
+
+bool CSR::WriteData(std::uint32_t addr, std::uint32_t value) {
+  // TODO: deal with privilege! (lower cannot access upper)
+  auto it = csrs_.find(addr);
+  if (it == csrs_.end()) {
+    // CSR does not exist, illegal instruction
+    return false;
+  }
+  else {
+    // TODO: with bit mask
+    // TODO: handle side effects
+    // TODO: write to epc and then misalligned?
+    // TODO: sync mstatus & sstatus
+    // reset hardwired CSR/bits
+    zero_ = 0;
+    return true;
+  }
 }
--- src/core/storage/csr.h
+++ src/core/storage/csr.h
@@ -4,27 +4,66 @@
 #include <unordered_map>
 #include <cstdint>
 
+#include "define/csr.h"
+#include "util/cast.h"
+
 // control and status registers
 class CSR {
  public:
   CSR();
 
-  // read data from CSR
-  std::uint32_t ReadData(std::uint32_t addr);
-  // write data to CSR
-  void WriteData(std::uint32_t addr, std::uint32_t value);
+  // update performance counter
+  void UpdateCounter();
+  // read data from CSR, returns false if failed
+  bool ReadData(std::uint32_t addr, std::uint32_t &value);
+  // write data to CSR, returns false if failed
+  bool WriteData(std::uint32_t addr, std::uint32_t value);
 
   // setters
-  void set_ext_int(bool ext_int) { ext_int_ = ext_int; }
+  void set_cur_priv(std::uint32_t cur_priv) { cur_priv_ = cur_priv; }
+  void set_mstatus(std::uint32_t mstatus) { mstatus_ = mstatus; }
+  void set_mepc(std::uint32_t mepc) { mepc_ = mepc; }
+  void set_mcause(std::uint32_t mcause) { mcause_ = mcause; }
+  void set_mtval(std::uint32_t mtval) { mtval_ = mtval; }
 
   // getters
-  std::uint32_t mtvec() const { return *mtvec_; }
-  std::uint32_t mepc() const { return *mepc_; }
+  // current privilege level
+  std::uint32_t cur_priv() const { return cur_priv_; }
+  // S-mode exception program counter
+  std::uint32_t sepc() const { return sepc_; }
+  // machine mode status
+  std::uint32_t mstatus() const { return mstatus_; }
+  // trap vector
+  std::uint32_t trap_vec() const {
+    if (mtvec_ & 0b11 == 1) {
+      return mtvec_ - 1 + (mcause_ & 0x7fffffff) * 4;
+    }
+    else {
+      return mtvec_;
+    }
+  }
+  // M-mode exception program counter
+  std::uint32_t mepc() const { return mepc_; }
+  // M-mode exception cause
+  std::uint32_t mcause() const { return mcause_; }
 
  private:
-  bool ext_int_;
-  std::unordered_map<std::uint32_t, std::uint32_t> csrs_;
-  std::uint32_t *mtvec_, *mepc_;
+  void InitCSR();
+  void InitMapping();
+
+  // current privilege level
+  std::uint32_t cur_priv_;
+  // CSR address mapping
+  std::unordered_map<std::uint32_t, std::uint32_t *> csrs_;
+  // CSR that hardwired to zero
+  std::uint32_t zero_;
+  // supervisor mode CSRs
+  std::uint32_t sstatus_, sscratch_, sepc_, satp_;
+  // machine mode CSRs
+  std::uint32_t mstatus_, misa_, mie_, mtvec_, mscratch_;
+  std::uint32_t mepc_, mcause_, mtval_, mip_;
+  // machine mode counters (64-bit)
+  std::uint64_t mcycle_, minstret_;
 };
 
 #endif  // RISKY32_CORE_STORAGE_CSR_H_
