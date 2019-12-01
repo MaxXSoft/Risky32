76cc038cf4b270b9ee9e370b08cc671d03d393ac
MaxXSoft <x@MaxXSoft.net>
2019-12-01 17:33:53 +0800

    (main) added peripheral 'CLINT'

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "core/core.h"
 #include "bus/bus.h"
 #include "peripheral/general/gpio.h"
+#include "peripheral/interrupt/clint.h"
 #include "peripheral/storage/ram.h"
 #include "peripheral/storage/rom.h"
 
@@ -41,6 +42,7 @@ int main(int argc, const char *argv[]) {
   auto rom = std::make_shared<ROM>();
   auto ram = std::make_shared<RAM>(65536);
   auto gpio = std::make_shared<GPIO>();
+  auto clint = std::make_shared<CLINT>();
   if (!rom->LoadBinary(argv[1])) {
     cerr << "error: failed to load file '" << argv[1] << "'" << endl;
     return 1;
@@ -51,12 +53,18 @@ int main(int argc, const char *argv[]) {
   bus->AddPeripheral(kMMIOAddrROM, rom);
   bus->AddPeripheral(kMMIOAddrRAM, ram);
   bus->AddPeripheral(kMMIOAddrGPIO, gpio);
+  bus->AddPeripheral(kMMIOAddrCLINT, clint);
 
   // initialize core
   Core core(bus);
+  core.set_timer_int(clint->timer_int());
+  core.set_soft_int(clint->soft_int());
   core.Reset();
 
   // run simulation
-  while (!gpio->halt()) core.NextCycle();
+  while (!gpio->halt()) {
+    core.NextCycle();
+    clint->UpdateTimer();
+  }
   return 0;
 }
