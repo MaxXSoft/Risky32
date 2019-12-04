b66e0714d86d13255d103b05cf5a145d9e29b4b5
MaxXSoft <x@MaxXSoft.net>
2019-12-04 16:05:56 +0800

    (expreval) added more details

--- src/debugger/expreval.h
+++ src/debugger/expreval.h
@@ -3,7 +3,7 @@
 
 #include <string_view>
 #include <ostream>
-#include <vector>
+#include <unordered_map>
 #include <string>
 #include <sstream>
 #include <cstdint>
@@ -13,16 +13,29 @@
 // expression evaluator
 class ExprEvaluator {
  public:
-  ExprEvaluator(Core &core) : core_(core) {}
+  ExprEvaluator(Core &core) : core_(core), next_id_(0) {}
 
   // evaluate expression with record
   bool Eval(std::string_view expr, std::uint32_t &ans);
   // evaluate expression
   bool Eval(std::string_view expr, std::uint32_t &ans, bool record);
+  // evaluate recorded expression by specific record id
+  bool Eval(std::uint32_t id, std::uint32_t &ans);
   // show register information
   void PrintRegInfo(std::ostream &os);
   // show CSR information
   void PrintCSRInfo(std::ostream &os);
+  // show expression info by specific record id
+  void PrintExprInfo(std::ostream &os, std::uint32_t id);
+
+  // remove specific record
+  void RemoveRecord(std::uint32_t id);
+  // remove all records
+  void Clear();
+
+  // getters
+  // next record id
+  std::uint32_t next_id() const { return records_.size(); }
 
  private:
   enum class Token {
@@ -44,7 +57,9 @@ class ExprEvaluator {
   // reference of emulation core
   Core &core_;
   // all stored records
-  std::vector<std::string> records_;
+  std::unordered_map<std::uint32_t, std::string> records_;
+  // next record id
+  std::uint32_t next_id_;
   // current expression
   std::istringstream iss;
   // last character
