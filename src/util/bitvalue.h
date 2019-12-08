76ec1e81d4ee54f4b1d73e2976da42ccad75a58d
MaxXSoft <x@MaxXSoft.net>
2019-12-08 10:52:58 +0800

    (bitvalue) modified some details

--- src/util/bitvalue.h
+++ src/util/bitvalue.h
@@ -5,64 +5,52 @@
 #include <cstddef>
 #include <cassert>
 
-#include "util/cast.h"
-
-// TODO: constexpr
-
 // represents a sequence of bits
-// can hold up to N bits
-template <std::size_t N>
+// can hold up to 'sizeof(std::size_t) * 8' bits
 class BitValue {
  public:
-  using ValueType = typename IntPtrType<N>::Type;
-
-  BitValue(ValueType value, std::size_t width)
+  BitValue(std::size_t value, std::size_t width)
       : value_(value), width_(width) {
-    assert(width_ && width_ <= N);
+    assert(width_ && width_ <= sizeof(std::size_t) * 8);
   }
 
   // get a single bit from current value
-  BitValue<N> Get(std::size_t i) { return Extract(i, i); }
+  BitValue Get(std::size_t i) { return Extract(i, i); }
 
   // extract a subsequence of bits from current value
-  BitValue<N> Extract(std::size_t hi, std::size_t lo) {
+  BitValue Extract(std::size_t hi, std::size_t lo) {
     assert(hi < width_ && hi >= lo);
     auto width = hi - lo + 1;
     auto mask = (1 << width) - 1;
-    return BitValue<N>((value_ >> lo) & mask, width);
+    return BitValue((value_ >> lo) & mask, width);
   }
 
   // concatenate with another 'BitValue'
-  template <std::size_t M>
-  auto Concat(const BitValue<M> &rhs) {
-    constexpr auto n = N > M ? N : M;
-    return BitValue<n>((value() << rhs.width()) | rhs.value(),
-                       width() + rhs.width());
+  BitValue Concat(const BitValue &rhs) {
+    return BitValue((value() << rhs.width()) | rhs.value(),
+                    width() + rhs.width());
   }
 
   // 'get' operator
-  BitValue<N> operator[](std::size_t i) { return Get(i); }
+  BitValue operator[](std::size_t i) { return Get(i); }
+  BitValue operator()(std::size_t i) { return Get(i); }
 
   // 'extract' operator
-  BitValue<N> operator()(std::size_t hi, std::size_t lo) {
+  BitValue operator()(std::size_t hi, std::size_t lo) {
     return Extract(hi, lo);
   }
 
   // 'concat' operator
-  template <std::size_t M>
-  auto operator|(const BitValue<M> &rhs) {
+  auto operator|(const BitValue &rhs) {
     return Concat(rhs);
   }
 
   // getters
-  ValueType value() const { return value_ & ((1 << width_) - 1); }
+  std::size_t value() const { return value_ & ((1 << width_) - 1); }
   std::size_t width() const { return width_; }
 
  private:
-  ValueType value_;
-  std::size_t width_;
+  std::size_t value_, width_;
 };
-
-using Bit32 = BitValue<32>;
 
 #endif  // RISKY32_UTIL_BITVALUE_H_
