aa7fc554a4b91122309557994e082da75f23fcc1
MaxXSoft <x@MaxXSoft.net>
2019-12-08 11:05:02 +0800

    (bitvalue) added 'constexpr' support

--- src/util/bitvalue.h
+++ src/util/bitvalue.h
@@ -9,16 +9,16 @@
 // can hold up to 'sizeof(std::size_t) * 8' bits
 class BitValue {
  public:
-  BitValue(std::size_t value, std::size_t width)
+  constexpr BitValue(std::size_t value, std::size_t width)
       : value_(value), width_(width) {
     assert(width_ && width_ <= sizeof(std::size_t) * 8);
   }
 
   // get a single bit from current value
-  BitValue Get(std::size_t i) { return Extract(i, i); }
+  constexpr BitValue Get(std::size_t i) const { return Extract(i, i); }
 
   // extract a subsequence of bits from current value
-  BitValue Extract(std::size_t hi, std::size_t lo) {
+  constexpr BitValue Extract(std::size_t hi, std::size_t lo) const {
     assert(hi < width_ && hi >= lo);
     auto width = hi - lo + 1;
     auto mask = (1 << width) - 1;
@@ -26,28 +26,30 @@ class BitValue {
   }
 
   // concatenate with another 'BitValue'
-  BitValue Concat(const BitValue &rhs) {
+  constexpr BitValue Concat(const BitValue &rhs) const {
     return BitValue((value() << rhs.width()) | rhs.value(),
                     width() + rhs.width());
   }
 
   // 'get' operator
-  BitValue operator[](std::size_t i) { return Get(i); }
-  BitValue operator()(std::size_t i) { return Get(i); }
+  constexpr BitValue operator[](std::size_t i) const { return Get(i); }
+  constexpr BitValue operator()(std::size_t i) const { return Get(i); }
 
   // 'extract' operator
-  BitValue operator()(std::size_t hi, std::size_t lo) {
+  constexpr BitValue operator()(std::size_t hi, std::size_t lo) const {
     return Extract(hi, lo);
   }
 
   // 'concat' operator
-  auto operator|(const BitValue &rhs) {
+  constexpr BitValue operator|(const BitValue &rhs) const {
     return Concat(rhs);
   }
 
   // getters
-  std::size_t value() const { return value_ & ((1 << width_) - 1); }
-  std::size_t width() const { return width_; }
+  constexpr std::size_t value() const {
+    return value_ & ((1 << width_) - 1);
+  }
+  constexpr std::size_t width() const { return width_; }
 
  private:
   std::size_t value_, width_;
