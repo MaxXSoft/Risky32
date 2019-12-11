8ea0b1a260b72345b3bdaee32440b6dc1fe21c84
MaxXSoft <x@MaxXSoft.net>
2019-12-11 09:56:07 +0800

    (bitvalue) modified some details

--- src/util/bitvalue.h
+++ src/util/bitvalue.h
@@ -1,61 +1,66 @@
 #ifndef RISKY32_UTIL_BITVALUE_H_
 #define RISKY32_UTIL_BITVALUE_H_
 
+#include <type_traits>
 #include <cstdint>
 #include <cstddef>
 #include <cassert>
 
 // represents a sequence of bits
-// can hold up to 'sizeof(std::size_t) * 8' bits
+// can hold up to 'sizeof(T) * 8' bits
+template <typename T>
 class BitValue {
  public:
-  constexpr BitValue(std::size_t value, std::size_t width)
+  static_assert(std::is_integral<T>::value);
+
+  constexpr BitValue(T value, std::size_t width)
       : value_(value), width_(width) {
-    assert(width_ && width_ <= sizeof(std::size_t) * 8);
+    assert(width_ && width_ <= sizeof(T) * 8);
   }
 
   // get a single bit from current value
-  constexpr BitValue Get(std::size_t i) const { return Extract(i, i); }
+  constexpr BitValue<T> Get(std::size_t i) const { return Extract(i, i); }
 
   // extract a subsequence of bits from current value
-  constexpr BitValue Extract(std::size_t hi, std::size_t lo) const {
+  constexpr BitValue<T> Extract(std::size_t hi, std::size_t lo) const {
     assert(hi < width_ && hi >= lo);
     auto width = hi - lo + 1;
     auto mask = (1 << width) - 1;
-    return BitValue((value_ >> lo) & mask, width);
+    return BitValue<T>((value_ >> lo) & mask, width);
   }
 
   // concatenate with another 'BitValue'
-  constexpr BitValue Concat(const BitValue &rhs) const {
-    return BitValue((value() << rhs.width()) | rhs.value(),
-                    width() + rhs.width());
+  constexpr BitValue<T> Concat(const BitValue<T> &rhs) const {
+    return BitValue<T>((value() << rhs.width()) | rhs.value(),
+                       width() + rhs.width());
   }
 
   // 'get' operator
-  constexpr BitValue operator[](std::size_t i) const { return Get(i); }
-  constexpr BitValue operator()(std::size_t i) const { return Get(i); }
+  constexpr BitValue<T> operator[](std::size_t i) const { return Get(i); }
+  constexpr BitValue<T> operator()(std::size_t i) const { return Get(i); }
 
   // 'extract' operator
-  constexpr BitValue operator()(std::size_t hi, std::size_t lo) const {
+  constexpr BitValue<T> operator()(std::size_t hi, std::size_t lo) const {
     return Extract(hi, lo);
   }
 
   // 'concat' operator
-  constexpr BitValue operator|(const BitValue &rhs) const {
+  constexpr BitValue<T> operator|(const BitValue<T> &rhs) const {
     return Concat(rhs);
   }
 
-  // boolean operator
-  constexpr operator bool() const { return value(); }
+  // conversion operator
+  constexpr operator T() const { return value(); }
 
   // getters
-  constexpr std::size_t value() const {
-    return value_ & ((1 << width_) - 1);
-  }
+  constexpr T value() const { return value_ & ((1 << width_) - 1); }
   constexpr std::size_t width() const { return width_; }
 
  private:
-  std::size_t value_, width_;
+  T value_;
+  std::size_t width_;
 };
+
+using BitValue32 = BitValue<std::uint32_t>;
 
 #endif  // RISKY32_UTIL_BITVALUE_H_
