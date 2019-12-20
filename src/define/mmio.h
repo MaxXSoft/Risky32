d6e6d42e3e180862dd5d5c51a3ff1bfa8ef2e6c6
MaxXSoft <x@MaxXSoft.net>
2019-12-20 10:43:43 +0800

    (mmio) added definition of flash address

--- src/define/mmio.h
+++ src/define/mmio.h
@@ -10,6 +10,7 @@ constexpr std::uint32_t kMMIOAddrROM      = kResetVector;
 constexpr std::uint32_t kMMIOAddrRAM      = 0x80000000;
 constexpr std::uint32_t kMMIOAddrGPIO     = 0x90000000;
 constexpr std::uint32_t kMMIOAddrCLINT    = 0x90010000;
+constexpr std::uint32_t kMMIOAddrFlash    = 0x90020000;
 constexpr std::uint32_t kMMIOAddrDebugger = 0xfffffff0;
 
 #endif  // RISKY32_DEFINE_MMIO_H_
